c90b47ed313c35a32c1e6856be1a4227427a6bb6
choonsikLove <101436974+choonsikLove@users.noreply.github.com>
2022-05-05 11:19:07 +0900

    Update ProductController.java

--- cafe_mw_project/src/main/java/com/web/controller/ProductController.java
+++ cafe_mw_project/src/main/java/com/web/controller/ProductController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ProductController {
    
-   @RequestMapping("product_all")
+   @RequestMapping(value="/product_all", method=RequestMethod.GET)
    public String product_all() {
       
       return "/product/product_all";
    }
    
 
-   @RequestMapping("product_goods")
+   @RequestMapping(value="/product_goods", method=RequestMethod.GET)
    public String product_goods() {
          
       return "/product/product_goods";
    }
    
-   @RequestMapping("product_gift")
+   @RequestMapping(value="/product_gift", method=RequestMethod.GET)
    public String product_gift() {
          
       return "/product/product_gift";
    }         
 
-   @RequestMapping("product_base")
+   @RequestMapping(value="/product_base", method=RequestMethod.GET)
    public String product_base() {
          
       return "/product/product_base";
